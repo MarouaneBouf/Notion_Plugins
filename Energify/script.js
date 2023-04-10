c516ba5e5583a36d7392c02c434406ad375d529e
Marouane Boufarouj <104838272+MarouaneBouf@users.noreply.github.com>
2023-04-10 12:17:24 +0000

    Update script.js

--- Energify/script.js
+++ Energify/script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let counter = parseInt(document.querySelector("#box").innerHTML);
+  let counter = parseInt(localStorage.getItem("counter")) || 0;
   let actual_value = document.querySelector("#box");
+  actual_value.innerHTML = counter;
+
   document.querySelector("#energy").addEventListener("click", function () {
     counter++;
+    localStorage.setItem("counter", counter);
     actual_value.innerHTML = counter;
   });
 });
